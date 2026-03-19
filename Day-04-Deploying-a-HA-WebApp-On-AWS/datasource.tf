a7c30dc6d697f3fe2b5c6fb162e628c00f8a81de
troistrois <franciscangeno1@gmail.com>
2026-03-19 18:11:57 +0300

    Started day 4

--- Day-04-Deploying-a-HA-WebApp-On-AWS/datasource.tf
+++ Day-04-Deploying-a-HA-WebApp-On-AWS/datasource.tf
@@ -1,13 +1,3 @@
 # Learning data sources
 data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
-
-output "region" {
-    description = "AWS region"
-    value = data.aws_region.current.name
-}
-
-output "AZs" {
-    description = "AZs in this region"
-    value = tolist(data.aws_availability_zones.available.names)
-}
