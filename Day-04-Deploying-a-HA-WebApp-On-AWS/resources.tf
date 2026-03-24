a4118d8cd5d615e6a7f32d26b2f5bebaacd66400
troistrois <franciscangeno1@gmail.com>
2026-03-24 16:14:01 +0300

    Added a listener rule and changed Subnet AZs

--- Day-04-Deploying-a-HA-WebApp-On-AWS/resources.tf
+++ Day-04-Deploying-a-HA-WebApp-On-AWS/resources.tf
@@ -26,8 +26,8 @@ resource "aws_subnet" "Day4_Subnet2" {
 }
 
 resource "aws_subnet" "Day4_Subnet3" {
-    availability_zone = data.aws_availability_zones.available.names[2]
-    cidr_block = var.Subnet1_CIDR
+    availability_zone = data.aws_availability_zones.available.names[0]
+    cidr_block = var.Subnet3_CIDR
     vpc_id = aws_vpc.Day4VPC.id
     map_public_ip_on_launch = true
     tags = {
@@ -36,9 +36,9 @@ resource "aws_subnet" "Day4_Subnet3" {
 }
 
 resource "aws_subnet" "Day4_Subnet4" {
-    availability_zone = data.aws_availability_zones.available.names[3]
+    availability_zone = data.aws_availability_zones.available.names[1]
     vpc_id = aws_vpc.Day4VPC.id
-    cidr_block = var.Subnet2_CIDR
+    cidr_block = var.Subnet4_CIDR
     map_public_ip_on_launch = true
     tags = {
         Name = "Day4-Subnet4"
@@ -52,11 +52,6 @@ resource "aws_internet_gateway" "IGW" {
     }
 }
 
-resource "aws_internet_gateway_attachment" "IGW_Attachment" {
-    internet_gateway_id = aws_internet_gateway.IGW.id
-    vpc_id = aws_vpc.Day4VPC.id
-}
-
 resource "aws_route_table" "PublicRouteTable_Day4" {
     vpc_id = aws_vpc.Day4VPC.id
 }
@@ -67,9 +62,24 @@ resource "aws_route" "Day4_Route" {
     gateway_id = aws_internet_gateway.IGW.id
 }
 
-resource "aws_route_table_association" "Day4_RTb_Association" {
-    gateway_id = aws_internet_gateway.IGW.id
-    route_table_id = aws_route_table.PublicRouteTable_Day4.id
+resource "aws_route_table_association" "ELB_Subnet1" {
+  subnet_id      = aws_subnet.Day4_Subnet1.id
+  route_table_id = aws_route_table.PublicRouteTable_Day4.id
+}
+
+resource "aws_route_table_association" "ELB_Subnet2" {
+  subnet_id      = aws_subnet.Day4_Subnet2.id
+  route_table_id = aws_route_table.PublicRouteTable_Day4.id
+}
+
+resource "aws_route_table_association" "ASG_Subnet1" {
+  subnet_id      = aws_subnet.Day4_Subnet3.id
+  route_table_id = aws_route_table.PublicRouteTable_Day4.id
+}
+
+resource "aws_route_table_association" "ASG_Subnet2" {
+  subnet_id      = aws_subnet.Day4_Subnet4.id
+  route_table_id = aws_route_table.PublicRouteTable_Day4.id
 }
 
 resource "aws_security_group" "ELB_sg" {
@@ -93,6 +103,12 @@ resource "aws_vpc_security_group_ingress_rule" "ELB_sg_ingress" {
     }
 }
 
+resource "aws_vpc_security_group_egress_rule" "ELB_sg_egress" {
+  security_group_id = aws_security_group.ELB_sg.id
+  cidr_ipv4 = var.IGW_destination_IP
+  ip_protocol = "-1"
+}
+
 resource "aws_security_group" "EC2_sg" {
     description = "Security group for the instances"
     vpc_id = aws_vpc.Day4VPC.id
@@ -114,6 +130,7 @@ resource "aws_vpc_security_group_ingress_rule" "EC2_sg_ingress" {
     }
 }
 
+
 resource "aws_lb" "ELB" {
     subnets = [aws_subnet.Day4_Subnet1.id,aws_subnet.Day4_Subnet2.id]
     security_groups = [aws_security_group.ELB_sg.id]
@@ -123,39 +140,84 @@ resource "aws_lb" "ELB" {
     }
 }
 
-resource "aws_lb_listener" "ELB_Listener" {
-    default_action {
-        target_group_arn = aws_lb_target_group.LBTargetGroup.arn
-        type = "forward"
-    }
-    load_balancer_arn = aws_lb.ELB.arn
-    port = var.ELB_Port
-    protocol = "HTTP"
-    tags = {
-        Name = "Day4-ELBListener"
-    }
-}
-
 resource "aws_lb_target_group" "LBTargetGroup" {
     name = var.Target_Group
-    port = var.ELB_Port
+    port = var.Server_Port
     protocol = "HTTP"
     vpc_id = aws_vpc.Day4VPC.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        healthy_threshold   = 2
+        unhealthy_threshold = 5
+        timeout = 3
+        interval = 15
+        matcher = "200"
+    }
     tags = {
         Name = "Day4-LBTargetGroup"
     }
 }
 
-resource "aws_launch_configuration" "AMI" {
-    image_id = var.EC2AMI
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.EC2_sg.id]
+resource "aws_lb_listener" "ELB_Listener" {
+  load_balancer_arn = aws_lb.ELB.arn
+  port              = var.ELB_Port
+  protocol          = "HTTP"
 
-    user_data = <<-EOF
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.LBTargetGroup.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "ELB_Listener_rule" {
+    listener_arn = aws_lb_listener.ELB_Listener.arn
+    priority = 100
+
+    condition {
+        path_pattern {
+          values = ["*"]
+        }
+    }
+
+    action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.LBTargetGroup.arn
+    }
+}
+
+resource "aws_vpc_security_group_egress_rule" "ELB_to_EC2" {
+  security_group_id = aws_security_group.ELB_sg.id
+  referenced_security_group_id = aws_security_group.EC2_sg.id
+  from_port = var.Server_Port
+  to_port = var.Server_Port
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "EC2_allow_all_outbound" {
+  security_group_id = aws_security_group.EC2_sg.id
+  cidr_ipv4 = var.IGW_destination_IP
+  ip_protocol = "-1"
+}
+
+resource "aws_launch_template" "AMI" {
+
+    name_prefix = "AMI-"
+    image_id = var.EC2AMI
+    instance_type = var.InstanceType
+
+    network_interfaces {
+        associate_public_ip_address = true
+        security_groups = [aws_security_group.EC2_sg.id]
+    }
+
+    user_data = base64encode(<<-EOF
                 #!/bin/bash
-                echo "Yaay! Welcome to my web server. I just Completed Day 03 of the Terraform Challenge." > index.html
+                echo "Yaay! Welcome to my web server. I just Completed Day 04 of the Terraform Challenge." > index.html
                 nohup busybox httpd -f -p ${var.Server_Port} &
                 EOF
+    )
 
     lifecycle {
         create_before_destroy = true
@@ -167,7 +229,14 @@ resource "aws_autoscaling_group" "ASG" {
     max_size = var.maxServers
     min_size = var.minServers
     vpc_zone_identifier = [aws_subnet.Day4_Subnet3.id, aws_subnet.Day4_Subnet4.id]
-    launch_configuration = aws_launch_configuration.AMI.name
+    
+    launch_template {
+        id = aws_launch_template.AMI.id
+        version = "$Latest"
+    }
+    target_group_arns = [aws_lb_target_group.LBTargetGroup.arn]
+    health_check_type = "ELB"
+    health_check_grace_period = 300
     tag {
         key = "Name"
         value = "Day4-ASG"
