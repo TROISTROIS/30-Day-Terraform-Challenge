b1ca761984e993a266119992c90e2d9bb01c130d
troistrois <franciscangeno1@gmail.com>
2026-03-27 13:58:47 +0300

    Added variables for the lambda

---
+++ Day-06-Understanding-and-Managing-Terraform-State-with-S3-and-DynamoDB/variables.tf
@@ -0,0 +1,11 @@
+variable "lambdafunction" {
+    description = "Name of the Lambda Function"
+    type = string
+    default = "Day-6-lambda-function"
+}
+
+variable "lambdarole" {
+    description = "Name of the Lambda Execution Role"
+    type = string
+    default = "Lambda-execution-Role"
+}
