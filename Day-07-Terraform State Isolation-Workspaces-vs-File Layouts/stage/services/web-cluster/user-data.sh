484364b27fd06352ec0715e9252e1a76ec1b7eec
troistrois <franciscangeno1@gmail.com>
2026-04-01 15:01:15 +0300

    Create user data script

---
+++ Day-07-Terraform State Isolation-Workspaces-vs-File Layouts/stage/services/web-cluster/user-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat > index.html <<EOF
+<h1>Day 07/30 of the Terraform Challenge !</h1>
+<p>DB address: ${db_address}</p>
+<p>DB port: ${db_port}</p>
+EOF
+nohup busybox httpd -f -p ${server_port} &
