eb48a31809471de46aa31069234c561c0b984cba
troistrois <franciscangeno1@gmail.com>
2026-04-23 18:09:45 +0300

    Fixed outputs

--- Day-08-Building-Reusable-Infrasructure-with-Terraform-Modules/stage/services/webserver-cluster/main.tf
+++ Day-08-Building-Reusable-Infrasructure-with-Terraform-Modules/stage/services/webserver-cluster/main.tf
@@ -1,3 +1,14 @@
+data "terraform_remote_state" "db" {
+    backend = "s3"
+    
+    config = {
+    bucket = "backend-bucket-zn"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    profile = "IAMAdmin-GEN"
+    region = "us-east-1"
+    }
+}
+
 provider "aws" {
     region = "us-east-1"
     profile = "IAMAdmin-GEN"
@@ -7,7 +18,7 @@ module "webserver-cluster" {
     source = "../../../Modules/Services/webserver-cluster"
 
     cluster_name = "webserver-day8"
-    db_remote_state_bucket = "backend-bucket-zn"
-    db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
     instance_type = "t2.micro"
+    db_endpoint = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
 }
