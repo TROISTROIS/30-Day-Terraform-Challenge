176b8f51475d95ae701ab6497f47e6cc6fb03c09
troistrois <franciscangeno1@gmail.com>
2026-04-27 16:14:50 +0300

    Configured stage environment

---
+++ Day-09-Advanced-Terraform-Modules-Versioning-Gotchas-and-MultiEnv-Reuse/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "IAMAdmin-GEN"
+}
+
+module "webserver" {
+    source = "../../../modules/services/webserver"
+
+    VPC_name = "Day9"
+    VPC_CIDR = "10.0.0.0/16"
+    Subnet1_CIDR = "10.0.0.0/24"
+    Subnet2_CIDR = "10.0.1.0/24"
+    Subnet3_CIDR = "10.0.2.0/24"
+    Subnet4_CIDR = "10.0.3.0/24"
+    EC2AMI = "ami-0ec10929233384c7f"
+    minServers = 2
+    maxServers = 5
+    InstanceType = "t2.micro"
+}
