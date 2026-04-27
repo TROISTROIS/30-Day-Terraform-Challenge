b4f40cee2ede8ab7ad062ba6e65a268e3dbed757
troistrois <franciscangeno1@gmail.com>
2026-04-27 16:15:52 +0300

    Refactored backend.tf

--- Day-09-Advanced-Terraform-Modules-Versioning-Gotchas-and-MultiEnv-Reuse/global/s3/backend.tf
+++ Day-09-Advanced-Terraform-Modules-Versioning-Gotchas-and-MultiEnv-Reuse/global/s3/backend.tf
@@ -1,10 +1,5 @@
 terraform {
     backend "s3" {
-        bucket = "s3-backend-91-bucket"
         key = "global/s3/terraform.tfstate"
-        region = "us-east-1"
-        profile = "IAMAdmin-GEN"
-        dynamodb_table = "dynamodb-remote-backend"
-        encrypt = true
     }
 }
