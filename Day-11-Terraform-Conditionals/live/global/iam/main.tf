8543f708a983cb27743bb2f0d62aaba03935ec73
troistrois <franciscangeno1@gmail.com>
2026-05-05 14:39:10 +0300

    Added conditional and loops

--- Day-11-Terraform-Conditionals/live/global/iam/main.tf
+++ Day-11-Terraform-Conditionals/live/global/iam/main.tf
@@ -4,9 +4,41 @@ provider "aws" {
 }
 
 
+resource "aws_iam_policy" "cloudwatch_read_only" {
+    name   = "cloudwatch-read-only"
+    policy = data.aws_iam_policy_document.cloudwatch_read_only_policy.json
+}
+
+data "aws_iam_policy_document" "cloudwatch_read_only_policy" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "cloudwatch:Describe*",
+            "cloudwatch:Get*",
+            "cloudwatch:List*"
+        ]
+        resources = ["*"]
+    }
+}
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+    name   = "cloudwatch-full-access"
+    policy = data.aws_iam_policy_document.cloudwatch_full_access_policy.json
+}
+
+data "aws_iam_policy_document" "cloudwatch_full_access_policy" {
+    statement {
+        effect    = "Allow"
+        actions   = ["cloudwatch:*"]
+        resources = ["*"]
+    }
+}
+
 module "IAMusers" {
-    source = "github.com/TROISTROIS/30-day-terraform-modules.git//landing-zone/iam-user?ref=v0.4.4"
+    source = "github.com/TROISTROIS/30-day-terraform-modules.git//landing-zone/iam-user?ref=v0.4.7"
     for_each = toset(var.user_names)
     user_name = each.value
-    give_dev_cloudwatch_full_access = false
+    give_dev_cloudwatch_full_access = true
+    cloudwatch_read_only_arn        = aws_iam_policy.cloudwatch_read_only.arn
+    cloudwatch_full_access_arn      = aws_iam_policy.cloudwatch_full_access.arn
 }
