f520288db049263a1cfdb7de81e4034d48bf4cef
troistrois <franciscangeno1@gmail.com>
2026-05-07 22:10:58 +0300

    Instance refresh on staging

---
+++ Day-12-Zero-Downtime-deployments-with-Terraform/live/stage/services/webserver-cluster/backend.tf
@@ -0,0 +1,5 @@
+# terraform {
+#     backend "s3" {
+#         key = "live/stage/services/webserver-cluster/terraform.tfstate"
+#     }
+# }
---
+++ Day-12-Zero-Downtime-deployments-with-Terraform/live/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    profile = "IAMAdmin-GEN"
+    region = "us-east-1"
+}
+
+module "webserver_cluster" {
+    source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/webserver?ref=v0.5.5"
+    VPC_name = "Day12-Stage"
+    VPC_CIDR = "10.0.0.0/16"
+    AZs = 2
+    newbits = 8
+    subnet_count = 4
+    day = "Day 12"
+    EC2AMI = "ami-0ec10929233384c7f"
+    minServers = 2
+    maxServers = 5
+    Environment = "Stage"
+    enable_autoscaling = false
+    ami = "ami-0ec10929233384c7f"
+}
