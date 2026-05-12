0ad40235ff40da0dbf72b478742b28d79cc0fdcd
troistrois <franciscangeno1@gmail.com>
2026-05-12 13:33:26 +0300

    Fixed prod variables

--- Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/prod/services/datasources/mysql/main.tf
+++ Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/prod/services/datasources/mysql/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 module "webserver_cluster" {
     source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/datasources/mysql?ref=v0.6.3"
-    environment = "stage"
-    prefix = "day13-stage"
+    environment = "prod"
+    prefix = "day13-prod"
     RDS_engine = "mysql"
-    dbname = "stage-DB"
+    dbname = "prod-DB"
     db_username = local.creds.username
     db_password = local.creds.password
 }
