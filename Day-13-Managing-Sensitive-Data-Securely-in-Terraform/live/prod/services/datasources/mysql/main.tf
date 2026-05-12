0a8d191b21456a53513c82ac949884e999b3a719
troistrois <franciscangeno1@gmail.com>
2026-05-12 15:23:49 +0300

    Fixed prefix error

--- Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/prod/services/datasources/mysql/main.tf
+++ Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/prod/services/datasources/mysql/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/datasources/mysql?ref=v0.6.3"
+    source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/datasources/mysql?ref=v0.6.4"
     environment = "prod"
     prefix = "day13-prod"
     RDS_engine = "mysql"
-    dbname = "prod-DB"
+    dbname = "prodDB"
     db_username = local.creds.username
     db_password = local.creds.password
 }
