e950d2074d107908db59d3c2e009ce76676da126
troistrois <franciscangeno1@gmail.com>
2026-05-12 15:28:21 +0300

    Modified dB name

--- Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/stage/services/datasources/mysql/main.tf
+++ Day-13-Managing-Sensitive-Data-Securely-in-Terraform/live/stage/services/datasources/mysql/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/datasources/mysql?ref=v0.6.3"
+    source = "github.com/TROISTROIS/30-day-terraform-modules.git//services/datasources/mysql?ref=v0.6.4"
     environment = "stage"
     prefix = "day13-stage"
     RDS_engine = "mysql"
-    dbname = "stage-DB"
+    dbname = "stageDB"
     db_username = local.creds.username
     db_password = local.creds.password
 }
