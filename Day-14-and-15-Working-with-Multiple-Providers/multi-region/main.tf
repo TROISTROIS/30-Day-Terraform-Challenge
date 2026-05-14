9c9d8f20eed487071e26bda6bb2b3dc75657be80
troistrois <franciscangeno1@gmail.com>
2026-05-14 16:18:56 +0300

    Deployed EC2 instances across 2 AZs

---
+++ Day-14-and-15-Working-with-Multiple-Providers/multi-region/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "instance_region1" {
+    provider = aws.region1
+    ami = data.aws_ami.ubuntu_region1.id
+    instance_type = "t2.nano"
+}
+
+resource "aws_instance" "instance_region2" {
+    provider = aws.region2
+    ami = data.aws_ami.ubuntu_region2.id
+    instance_type = "t2.nano"
+}
