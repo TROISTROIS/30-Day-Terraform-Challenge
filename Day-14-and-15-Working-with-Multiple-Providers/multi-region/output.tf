353641e5e9d9916044fab349f7831791713ad9d9
troistrois <franciscangeno1@gmail.com>
2026-05-14 16:19:30 +0300

    Verified outputs

---
+++ Day-14-and-15-Working-with-Multiple-Providers/multi-region/output.tf
@@ -0,0 +1,19 @@
+output "region1" {
+    description = "The name of Region 1"
+    value = data.aws_region.region_1.name
+}
+
+output "region2" {
+    description = "The name of Region 2"
+    value = data.aws_region.region_2
+}
+
+output "instance_region1_az" {
+    description = "The name of AZ in region 1"
+    value = aws_instance.instance_region1.availability_zone
+}
+
+output "instance_region2_az" {
+    description = "The name of AZ in region 2"
+    value = aws_instance.instance_region2.availability_zone
+}
