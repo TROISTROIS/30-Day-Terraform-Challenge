ac2a91d28a959fe18566cd77285a08e0e2e8f28e
troistrois <franciscangeno1@gmail.com>
2026-05-14 16:19:12 +0300

    Defined data sources

---
+++ Day-14-and-15-Working-with-Multiple-Providers/multi-region/data.tf
@@ -0,0 +1,31 @@
+data "aws_region" "region_1" {
+    provider = aws.region1
+}
+
+data "aws_region" "region_2" {
+    provider = aws.region2
+}
+
+data "aws_ami" "ubuntu_region1" {
+    provider = aws.region1
+
+    most_recent = true
+    owners = ["099720109477"]
+
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-26.04-amd64-server-*"]
+    }
+}
+
+data "aws_ami" "ubuntu_region2" {
+    provider = aws.region2
+
+    most_recent = true
+    owners = ["099720109477"]
+
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-26.04-amd64-server-*"]
+    }
+}
