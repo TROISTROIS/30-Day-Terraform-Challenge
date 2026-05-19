1cb689a621dc41104afbf9084246bdf18c339571
troistrois <franciscangeno1@gmail.com>
2026-05-19 13:03:41 +0300

    Multi-account without modules

--- Day-15-Working-with-Multiple-Providers-Part-2/multi-account/outputs.tf
+++ Day-15-Working-with-Multiple-Providers-Part-2/multi-account/outputs.tf
@@ -2,6 +2,7 @@ output "parent_account" {
 value = data.aws_caller_identity.parent.account_id
 description = "The ID of the parent AWS account"
 }
+
 output "child_account" {
 value = data.aws_caller_identity.child.account_id
 description = "The ID of the child AWS account"
