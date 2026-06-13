8dd64f603a48278ff670f9121bdc40d4db671997
troistrois <franciscangeno1@gmail.com>
2026-06-13 18:36:49 +0300

    Added assume_role as a nested block to the backend s3 block

--- Day-16-Building-Production-Grade-Infrastructure/live/prod/backend.tf
+++ Day-16-Building-Production-Grade-Infrastructure/live/prod/backend.tf
@@ -1,5 +1,8 @@
 # terraform {
 #     backend "s3" {
 #         key = "live/prod/terraform.tfstate"
+#         assume_role = {
+#             role_arn = "arn:aws:iam::966392475043:role/CrossAccountRole"
+#             }
 #     }
 # }
